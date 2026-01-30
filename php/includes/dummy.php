14d3eae2fc9b34b07d971b0df70f31da59f0c15d
Emil Berberg Olsen <emil@NETT-OPP.local>
2026-01-30 15:50:41 +0100

    small fix

--- php/includes/dummy.php
+++ php/includes/dummy.php
@@ -2,6 +2,7 @@
 if (!defined("ABSPATH")) {
     exit();
 }
+
 add_action("rest_api_init", function () {
     register_rest_route("custom/v1", "/dummy", [
         "methods" => "GET",
@@ -10,6 +11,9 @@ add_action("rest_api_init", function () {
                 "message" => "Hello from WordPress 👋",
                 "status" => "ok",
             ];
+        },
+        "permission_callback" => function () {
+            return current_user_can("manage_options");
         },
     ]);
 });
